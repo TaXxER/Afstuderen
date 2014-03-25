d0eeb7bc1ab66883ce12200ef252b7ab1657356e
Niek Tax <niek.tax@avanade.com>
2014-03-25 12:08:49 +0100

    winning numbers kunnen worden berekend :)

--- method_selection/winning_number.R
+++ method_selection/winning_number.R
@@ -1,2 +1,4 @@
 setwd("C:/Git-data/Afstuderen/method_selection")
-ltr_data = read.csv("ndcg5_results.csv")
+ltr_data = read.csv("ndcg10_results.csv")
+temp <- ltr_data[,c(2:(length(ltr_data)-2))]
+winnum <- apply(temp, 2, function(x) sapply(x, function(y) if(is.na(y)){0}else{sum(x[!is.na(x)]<y)}))
