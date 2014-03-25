377cdee873a3169043751c034dccc5e374826c8b
Niek Tax <niek.tax@avanade.com>
2014-03-25 13:41:12 +0100

    winning number en ideal winning number geimplementeerd

--- method_selection/winning_number.R
+++ method_selection/winning_number.R
@@ -1,4 +1,6 @@
 setwd("C:/Git-data/Afstuderen/method_selection")
 ltr_data = read.csv("ndcg10_results.csv")
 temp <- ltr_data[,c(2:(length(ltr_data)-2))]
-winnum <- apply(temp, 2, function(x) sapply(x, function(y) if(is.na(y)){0}else{sum(x[!is.na(x)]<y)}))
+winnum <- apply(temp, 2, function(x) sapply(x, function(y) if(is.na(y)){NA}else{sum(x[!is.na(x)]<y)}))
+winnum <- data.frame(winnum)
+ideal_winnum <- apply(winnum, 2, function(x) sapply(x, function(y) if(is.na(y)){0}else{if(length(x[!is.na(x)]) == 0){0}else{max(x[!is.na(x)])}}))
