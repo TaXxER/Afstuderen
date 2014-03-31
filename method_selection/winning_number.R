aa68c95c137a4c68ec03e56c66980f18ecf5c5d6
Niek Tax <niek.tax@avanade.com>
2014-03-31 12:19:45 +0200

    werk aan combined + bugfixes

--- method_selection/winning_number.R
+++ method_selection/winning_number.R
@@ -1,62 +1,90 @@
 setwd("C:/Git-data/Afstuderen/method_selection")
 require(ggplot2)
-
+require(plyr)
 
 ### MAP ###
+#read data
 map_data = read.csv("map_results.csv")
-temp <- map_data[,c(2:(length(map_data)-2))]
-map_winnum <- apply(temp, 2, function(x) sapply(x, function(y) if(is.na(y)){NA}else{sum(x[!is.na(x)]<y)}))
+
+#data transformations
+map_temp <- map_data[,c(2:(length(map_data)-2))]
+map_temp <- apply(map_temp, 2, function(x) if(sum(!is.na(x))>1) {x}else{"ILLEGAL"} )
+map_temp <- map_temp[map_temp!="ILLEGAL"]
+map_temp <- data.frame(map_temp)
+
+# summation and normalization operations
+map_winnum <- apply(map_temp, 2, function(x) sapply(x, function(y) if(is.na(y)){NA}else{sum(x[!is.na(x)]<y)}))
 map_winnum <- data.frame(map_winnum)
 map_ideal_winnum <- apply(map_winnum, 2, function(x) sapply(x, function(y) if(is.na(y)){0}else{if(length(x[!is.na(x)]) == 0){0}else{max(x[!is.na(x)])}}))
 map_summed_winnum <- apply(map_winnum, 1, function(x) sum(x,na.rm=TRUE))
 map_summed_ideal_winnum <- apply(map_ideal_winnum, 1, function(x) sum(x,na.rm=TRUE))
 map_norm_winnum <- map_summed_winnum/map_summed_ideal_winnum
-map_nr_measurements <- apply(temp,1,function(x) sum(!is.na(x)))
+map_nr_measurements <- apply(map_temp,1,function(x) sum(!is.na(x)))
 map_output <- data.frame("Method" = map_data[,1], "Normalised_winning_number" = map_norm_winnum, "Nr_measurements"= map_nr_measurements)
 
 # add ID's to output
 map_output$id <- seq_len(nrow(map_output))
-nc <- ncol(map_outputoutput)
-map_output <- map_outputoutput[, c(nc, 1:(nc-1))]
+map_output <- map_output[, c(ncol(map_output), 1:(ncol(map_output)-1))]
 
 
 ### NDCG@5 ###
+# read data
 ndcg5_data = read.csv("ndcg5_results.csv")
-temp <- ndcg5_data[,c(2:(length(ndcg5_data)-2))]
-ndcg5_winnum <- apply(temp, 2, function(x) sapply(x, function(y) if(is.na(y)){NA}else{sum(x[!is.na(x)]<y)}))
+
+# data transformations
+ndcg5_temp <- ndcg5_data[,c(2:(length(ndcg5_data)-2))]
+ndcg5_temp <- apply(ndcg5_temp, 2, function(x) if(sum(!is.na(x))>1) {x}else{"ILLEGAL"} )
+ndcg5_temp <- ndcg5_temp[ndcg5_temp!="ILLEGAL"]
+ndcg5_temp <- data.frame(ndcg5_temp)
+
+# summation and normalization operations
+ndcg5_winnum <- apply(ndcg5_temp, 2, function(x) sapply(x, function(y) if(is.na(y)){NA}else{sum(x[!is.na(x)]<y)}))
 ndcg5_winnum <- data.frame(ndcg5_winnum)
 ndcg5_ideal_winnum <- apply(ndcg5_winnum, 2, function(x) sapply(x, function(y) if(is.na(y)){0}else{if(length(x[!is.na(x)]) == 0){0}else{max(x[!is.na(x)])}}))
 ndcg5_summed_winnum <- apply(ndcg5_winnum, 1, function(x) sum(x,na.rm=TRUE))
 ndcg5_summed_ideal_winnum <- apply(ndcg5_ideal_winnum, 1, function(x) sum(x,na.rm=TRUE))
 ndcg5_norm_winnum <- ndcg5_summed_winnum/ndcg5_summed_ideal_winnum
-ndcg5_nr_measurements <- apply(temp,1,function(x) sum(!is.na(x)))
+ndcg5_nr_measurements <- apply(ndcg5_temp,1,function(x) sum(!is.na(x)))
 ndcg5_output <- data.frame("Method" = ndcg5_data[,1], "Normalised_winning_number" = ndcg5_norm_winnum, "Nr_measurements"= ndcg5_nr_measurements)
-
+              
 # add ID's to output
 ndcg5_output$id <- seq_len(nrow(ndcg5_output))
-nc <- ncol(ndcg5_output)
-ndcg5_output <- ndcg5_output[, c(nc, 1:(nc-1))]
+ndcg5_output <- ndcg5_output[, c(ncol(ndcg5_output), 1:(ncol(ndcg5_output)-1))]
 
 
 ### NDCG@10 ###
+#read data
 ndcg10_data = read.csv("ndcg10_results.csv")
-temp <- ndcg10_data[,c(2:(length(ndcg10_data)-2))]
-ndcg10_winnum <- apply(temp, 2, function(x) sapply(x, function(y) if(is.na(y)){NA}else{sum(x[!is.na(x)]<y)}))
+
+# data transformations
+ndcg10_temp <- ndcg10_data[,c(2:(length(ndcg10_data)-2))]
+ndcg10_temp <- apply(ndcg10_temp, 2, function(x) if(sum(!is.na(x))>1) {x}else{"ILLEGAL"} )
+ndcg10_temp <- ndcg10_temp[ndcg10_temp!="ILLEGAL"]
+ndcg10_temp <- data.frame(ndcg10_temp)
+
+ndcg10_winnum <- apply(ndcg10_temp, 2, function(x) sapply(x, function(y) if(is.na(y)){NA}else{sum(x[!is.na(x)]<y)}))
 ndcg10_winnum <- data.frame(ndcg10_winnum)
 ndcg10_ideal_winnum <- apply(ndcg10_winnum, 2, function(x) sapply(x, function(y) if(is.na(y)){0}else{if(length(x[!is.na(x)]) == 0){0}else{max(x[!is.na(x)])}}))
 ndcg10_summed_winnum <- apply(ndcg10_winnum, 1, function(x) sum(x,na.rm=TRUE))
 ndcg10_summed_ideal_winnum <- apply(ndcg10_ideal_winnum, 1, function(x) sum(x,na.rm=TRUE))
 ndcg10_norm_winnum <- ndcg10_summed_winnum/ndcg10_summed_ideal_winnum
-ndcg10_nr_measurements <- apply(temp,1,function(x) sum(!is.na(x)))
+ndcg10_nr_measurements <- apply(ndcg10_temp,1,function(x) sum(!is.na(x)))
 ndcg10_output <- data.frame("Method" = ndcg10_data[,1], "Normalised_winning_number" = ndcg10_norm_winnum, "Nr_measurements" = ndcg10_nr_measurements)
 
 # add ID's to output
 ndcg10_output$id <- seq_len(nrow(ndcg10_output))
-nc <- ncol(ndcg10_output)
-ndcg10_output <- ndcg10_output[, c(nc, 1:(nc-1))]
+ndcg10_output <- ndcg10_output[, c(ncol(ndcg10_output), 1:(ncol(ndcg10_output)-1))]
 
 ### COMBINED ###
-# TODO #
+combined_data <- data.frame(Method=character(), Summed_winnum=numeric(), Summed_ideal_winnum=numeric())
+combined_data <- rbind.data.frame(combined_data, unname(cbind(as.character(map_data[,1]),as.integer(map_summed_winnum),as.integer(map_summed_ideal_winnum))))
+combined_data <- rbind.data.frame(combined_data, unname(cbind(as.character(ndcg5_data[,1]),as.integer(ndcg5_summed_winnum),as.integer(ndcg5_summed_ideal_winnum))))
+combined_data <- rbind.data.frame(combined_data, unname(cbind(as.character(ndcg10_data[,1]),as.integer(ndcg10_summed_winnum),as.integer(ndcg10_summed_ideal_winnum))))
+#combined_data$V2 <- as.numeric(combined_data$V2)
+#combined_data$V3 <- as.numeric(combined_data$V3)
+#combined_aggregate_winnum <- aggregate(V2~V1,combined_data,sum)
+#combined_aggregate_ideal_winnum <- aggregate(V3~V1,combined_data,sum)
+#combined_norm_winnum <- combined_aggregate_winnum/combined_aggregate_ideal_winnum
 
 ### PLOT ###
 ### ADJUST HERE ###
