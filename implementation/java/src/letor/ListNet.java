aac4b4a57e53b8e1ce6a256a33106db57e7f22a3
Niek Tax <niek.tax@avanade.com>
2014-05-07 12:03:05 +0200

    mapreduce meuk verwijderd

--- implementation/java/src/letor/ListNet.java
+++ implementation/java/src/letor/ListNet.java
@@ -1,20 +1,13 @@
 package letor;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.pig.PigServer;
+
+import java.io.IOException;
+
 /**
  * MapReduce implementation of the ListNet algorithm
  */
-import java.io.IOException;
-import java.util.*;
-
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.conf.*;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapreduce.*;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-import org.apache.pig.PigServer;
 
 public class ListNet {
     // Initialise hyper-parameters
@@ -27,38 +20,12 @@ public class ListNet {
     private static final int DIM        = 45;
     private static final int QUERIES    = 20;
 
-    public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
-        private final static IntWritable one = new IntWritable(1);
-        private Text word = new Text();
-
-        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-            String line = value.toString();
-            StringTokenizer tokenizer = new StringTokenizer(line);
-            while (tokenizer.hasMoreTokens()) {
-                word.set(tokenizer.nextToken());
-                context.write(word, one);
-            }
-        }
-    }
-
-    public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
-
-        public void reduce(Text key, Iterable<IntWritable> values, Context context)
-                throws IOException, InterruptedException {
-            int sum = 0;
-            for (IntWritable val : values) {
-                sum += val.get();
-            }
-            context.write(key, new IntWritable(sum));
-        }
-    }
-
     public static void main(String[] args) throws Exception {
         // Connect to Pig
         PigServer pigServer = new PigServer("local");
         pigServer.registerQuery("register 'hdfs://localhost:8020/pig/udf/piggybank.jar'");
 
-        // Initialise model parameters
+        // Initialise internal model parameters
         double[] w        = new double[DIM];
         double[] gradient = new double[DIM];
         double   loss     = 0.0;
@@ -91,26 +58,9 @@ public class ListNet {
                 */
             }
         }
-        Configuration conf = new Configuration();
-
-        Job job = new Job(conf, "listnet");
-
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
-
-        job.setMapperClass(Map.class);
-        job.setReducerClass(Reduce.class);
-
-        job.setInputFormatClass(TextInputFormat.class);
-        job.setOutputFormatClass(TextOutputFormat.class);
-
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-
-        job.waitForCompletion(true);
     }
 
-    public static void gradientDescent(PigServer pigServer) throws IOException{
+    public static void gradientDescent(PigServer pigServer) throws IOException {
         pigServer.registerQuery("A = LOAD 'input/ohsumed/Fold1/train.txt' using PigStorage(' ');");
         pigServer.registerQuery("");
     }
