43a4a9b6343c7afecf86903b9741ae9246bf91c7
Niek Tax <niek.tax@avanade.com>
2014-09-08 21:30:57 +0200

    import gefixt

--- measurements/single_graph.R
+++ measurements/single_graph.R
@@ -1,6 +1,7 @@
 setwd("C:/Git-data/Afstuderen/measurements")
 library(ggplot2)
 library(mgcv)
+library(stringr)
 library(scales)
 measure = read.csv("raw_data.csv") # read csv file
 name <- paste(measure$serial.parallel, measure$nodeCount, sep = " ")
@@ -19,10 +20,10 @@ getName <- function(s){
 		)
 	)
 }
-measure$seconds <- measure$trainTime/60
+
 name <- unlist(lapply(name, getName))
+measure$seconds <- measure$trainTime/1000
 measure$serial.parallel <- name
-maxTime <- max(measure$trainTime)
 d <- ggplot(data=measure, aes(x=dataSize, y=seconds, colour=serial.parallel, shape=serial.parallel))
 d <- d + geom_point(size=2) + stat_smooth(se=FALSE, method = "glm", formula = y ~ exp(x)) + 
 	scale_x_log10() + theme_bw() + 
@@ -32,7 +33,7 @@ d <- d + geom_point(size=2) + stat_smooth(se=FALSE, method = "glm", formula = y 
 		panel.border     = element_blank(),
 		panel.background = element_blank(),
 		axis.line = element_line(color='black')
-	) + scale_y_continuous(expand = c(0.001, maxTime/10000))+	
+	) + scale_y_continuous(expand = c(0, 0))+	
 	xlab("Dataset size (in Byte)") +
 	ylab("Training iteration time (in Seconds)") +
 	labs(colour = "Execution mode", shape = "Execution mode") 
