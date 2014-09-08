d173ab85135bc29366335d3d89433bcc78fef344
Niek Tax <niek.tax@avanade.com>
2014-09-08 14:32:13 +0200

    mooiere visualisatie

--- measurements/visualisation.R
+++ measurements/visualisation.R
@@ -6,5 +6,11 @@ name <- paste(measure$serial.parallel, measure$nodeCount, sep = " ")
 name <- ifelse(name=="Serial NA", "Serial", name)
 measure$serial.parallel <- name
 d <- ggplot(data=measure, aes(x=dataSize, y=trainTime, colour=serial.parallel))
-d <- d + geom_point() + geom_smooth() + scale_x_log10()
+d <- d + geom_point() + geom_smooth(fill=NA) + 
+	scale_x_log10() + theme_bw() + opts(
+		panel.grid.major = theme_blank(),
+		panel.grid.minor = theme_blank(),
+		panel.border     = theme_blank(),
+		panel.background = theme_blank()
+	)
 d
