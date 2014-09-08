8ad52189fe488ff16c214e75b736f35dcba29985
Niek Tax <niek.tax@avanade.com>
2014-09-08 21:12:55 +0200

    faceted graph

---
+++ measurements/faceted_graph.R
@@ -0,0 +1,40 @@
+setwd("C:/Git-data/Afstuderen/measurements")
+library(ggplot2)
+library(mgcv)
+library(scales)
+measure = read.csv("raw_data.csv") # read csv file
+name <- paste(measure$serial.parallel, measure$nodeCount, sep = " ")
+
+getName <- function(s){
+	return(ifelse(grepl("^Parallel (\\d)+$", s), 
+			paste(
+				c(
+					"Cluster: ", 
+					str_extract(s, "\\d+"), 
+					" data nodes"
+				),
+				collapse = ''
+			), 
+			"Serial"
+		)
+	)
+}
+measure$seconds <- measure$trainTime/60
+name <- unlist(lapply(name, getName))
+measure$serial.parallel <- name
+maxTime <- max(measure$trainTime)
+d <- ggplot(data=measure, aes(x=dataSize, y=seconds, colour=serial.parallel))
+d <- d + geom_point(size=2) + stat_smooth(level=0.99, method = "glm", formula = y ~ exp(x)) + 
+	scale_x_log10() + theme_bw() + 
+	theme(
+		panel.grid.major = element_blank(),
+		panel.grid.minor = element_blank(),
+		panel.border     = element_blank(),
+		panel.background = element_blank(),
+		axis.line = element_line(color='black')
+	) + scale_y_continuous(expand = c(0.001, maxTime/10000))+	
+	facet_wrap(~serial.parallel)+
+	xlab("Dataset size (in Byte)") +
+	ylab("Training iteration time (in Seconds)") +
+	labs(colour = "Execution mode") 
+ggplot_build(d)
