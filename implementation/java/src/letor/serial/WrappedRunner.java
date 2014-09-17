2ce25a495bd38eb24a7e4b44b6d69e7c381ec8d7
Niek Tax <niek.tax@avanade.com>
2014-09-17 16:46:01 +0200

    ranking accuracy measurements for validation

--- implementation/java/src/letor/serial/WrappedRunner.java
+++ implementation/java/src/letor/serial/WrappedRunner.java
@@ -12,20 +12,22 @@ import letor.serial.util.Measurement;
 public class WrappedRunner {
 
     public static void main(String[] args){
-        AbstractParameterizedRanker handler             = new SmoothRankHandler();
-        DataSets.DataSet            dataset             = DataSets.DataSet.MSLR_WEB10K;
+        Integer                     folds               = 1;
+        Integer                     iterations          = 5;
+        AbstractParameterizedRanker handler             = new ListNetHandler();
+        DataSets.DataSet            dataset             = DataSets.DataSet.MSLR_WEB30K;
         Integer                     duplicationNumber   = 1; // Only relevant in case of dataset Custom
 
         FoldRunHandler ltrWrapper = null;
         if(dataset == DataSets.DataSet.CUSTOM)
-            ltrWrapper = new FoldRunHandler(handler, DataSets.DataSet.CUSTOM, duplicationNumber,  1, 1); // Method, dataset, folds, iterations
+            ltrWrapper = new FoldRunHandler(handler, DataSets.DataSet.CUSTOM, duplicationNumber,  folds, iterations);
         else
-            ltrWrapper = new FoldRunHandler(handler, dataset, 1, 1); // Method, dataset, folds, iterations
+            ltrWrapper = new FoldRunHandler(handler, dataset, folds, iterations);
         Measurement measurement = ltrWrapper.averageScore();
-        System.out.println("Preprocessing: "+measurement.getPreprocessingTime());
-        System.out.println("Train:         "+measurement.getTrainingTime());
-        System.out.println("Test:          "+measurement.getTestTime());
-        System.out.println("Total time:    "+measurement.getTotalTime());
-        System.out.println("NDCG@10:       "+measurement.getEvaluationResult()+" ms");
+        System.out.println("Preprocessing: "+measurement.getPreprocessingTime()+" ms");
+        System.out.println("Train:         "+measurement.getTrainingTime()+" ms");
+        System.out.println("Test:          "+measurement.getTestTime()+" ms");
+        System.out.println("Total time:    "+measurement.getTotalTime()+" ms");
+        System.out.println("NDCG@10:       "+measurement.getEvaluationResult());
     }
 }
